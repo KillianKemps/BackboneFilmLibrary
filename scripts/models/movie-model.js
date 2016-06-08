a7ab9784a08ff3e3615e6acaa77c3ae50f21cf56
Killian Kemps <killian.kemps@etu-webschoolfactory.fr>
2016-06-08 16:41:07 +0200

    Add movie to movie collection

--- scripts/models/movie-model.js
+++ scripts/models/movie-model.js
@@ -3,6 +3,8 @@ var MovieModel = Backbone.Model.extend({
   defaults: {
     title: '',
     poster: 'https://placekitten.com/200/200',
+    type: '',
+    notation: 0,
     seen: false
   }
 
--- scripts/views/movie-list.js
+++ scripts/views/movie-list.js
@@ -3,10 +3,35 @@ var MovieListView = Backbone.View.extend({
   el: '#app',
 
   events: {
+    'submit form': 'addMovie',
   },
 
+  addMovie: function(event) {
+    // Kill submit event
+    event.preventDefault();
+
+    var $form = $(event.currentTarget);
+    var movieTitle = $form.find('.movie-title').val();
+    var moviePoster = $form.find('.movie-poster').val();
+    var movieType = $form.find('.movie-type').val();
+    var movieNotation = $form.find('.movie-notation').val();
+
+    var newMovieModel = new MovieModel({
+      title: movieTitle,
+      poster: moviePoster,
+      type: movieType,
+      notation: parseInt(movieNotation)
+    });
+
+    this.myMovieCollection.add(newMovieModel);
+
+    this.render();
+  },
 
   initialize: function() {
+    // Set movie instance
+    this.myMovieCollection = new MovieCollection();
+
     this.render();
   },
 
